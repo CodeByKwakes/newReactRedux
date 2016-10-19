8995d15f0818f372b231e7ba91803b58d058bfd7
KwakesProject <kwakes@me.com>
2016-10-19 13:31:05 +0100

    term is a property of the state object used in the tutorial

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = { term: '' }
   }
-  
+
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />
+    return (
+      <div>
+        <input onChange={(event) => this.setState({ term: event.target.value })} />
+      </div>
+    )
   }
 }
 
